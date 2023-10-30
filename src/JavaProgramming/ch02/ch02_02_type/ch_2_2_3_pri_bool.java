576fc7e5e35691d05a1ea7707b825fc4af2ff02e
raccoonRyu <rjhwind94@gmail.com>
2023-10-30 13:39:41 +0900

    CH 2-2 자료형(논리) 소스 커밋

---
+++ src/JavaProgramming/ch02/ch02_02_type/ch_2_2_3_pri_bool.java
@@ -0,0 +1,13 @@
+package JavaProgramming.ch02.ch02_02_type;
+
+public class ch_2_2_3_pri_bool {
+    public static void main(String[] args) {
+        boolean b1 = true; // 논리형은 true와 false 두 개의 값만을 사용할 수 있다.
+
+        if(b1) {
+            System.out.println("참입니다.");
+        } else {
+            System.out.println("거짓입니다.");
+        }
+    }
+}
