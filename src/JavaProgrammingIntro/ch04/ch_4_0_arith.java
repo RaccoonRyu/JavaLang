965a5baa1a931a4b915f50118481e1334519993c
RaccoonRyu <rjhwind94@gmail.com>
2023-11-22 10:56:15 +0900

    4장 산술 연산자 예제 작성

---
+++ src/JavaProgrammingIntro/ch04/ch_4_0_arith.java
@@ -0,0 +1,14 @@
+package JavaProgrammingIntro.ch04;
+
+public class ch_4_0_arith {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 3;
+
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = " + (num1 - num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+        System.out.println("num1 / num2 = " + (num1 / num2)); // 나누기 연산. 결과 2 출력
+        System.out.println("num1 % num2 = " + (num1 % num2)); // 나머지 연산. 결과 1 출력
+    }
+}
