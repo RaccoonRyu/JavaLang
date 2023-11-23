abf4e6f5c4e8e34ddace5098b7b95319fb29910d
RaccoonRyu <rjhwind94@gmail.com>
2023-11-23 09:42:05 +0900

    2장 논리형 예제 작성

---
+++ src/JavaProgrammingIntro/ch02/ch_2_2_boolean.java
@@ -0,0 +1,14 @@
+package JavaProgrammingIntro.ch02;
+
+public class ch_2_2_boolean {
+    public static void main(String[] args) {
+        boolean check1 = true; // 직접 값 대입
+        boolean check2 = false;
+        boolean check3 = (1 < 2); // 연산의 결과를 값으로 대입. true
+
+        System.out.println(check1);
+        System.out.println(check2);
+        System.out.println(check3);
+        System.out.println(1 > 2); // 연산 결과 출력. false
+    }
+}
