ad6be5c1fb4895766bf6b8bd0ddbdc4607debcb8
RaccoonRyu <rjhwind94@gmail.com>
2023-11-23 09:40:45 +0900

    2장 정수형 예제 작성

---
+++ src/JavaProgrammingIntro/ch02/ch_2_0_intType.java
@@ -0,0 +1,15 @@
+package JavaProgrammingIntro.ch02;
+
+public class ch_2_0_intType {
+    public static void main(String[] args) {
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 4;
+        long num4 = 8; // 초기화된 각 숫자는 자료형의 byte 수
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
