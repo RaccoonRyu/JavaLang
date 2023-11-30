07ac4a5744216123aac68495b6ed984d62d0126f
raccoonRyu <rjhwind94@gmail.com>
2023-11-30 22:33:25 +0900

    6장 switch String 조건 예제 작성

---
+++ src/JavaProgrammingIntro/ch06/ch_6_3_switchString.java
@@ -0,0 +1,22 @@
+package JavaProgrammingIntro.ch06;
+
+public class ch_6_3_switchString {
+    public static void main(String[] args) {
+        String name = "홍길동";
+
+        switch(name) // 조건식에 문자열 사용
+        {
+            case "홍길동":
+                System.out.println("제 이름은 홍길동입니다.");
+                break;
+            case "전우치":
+                System.out.println("제 이름은 전우치입니다.");
+                break;
+            case "손오공":
+                System.out.println("제 이름은 손오공입니다.");
+                break;
+            default:
+                System.out.println("같은 이름이 없습니다.");
+        }
+    }
+}
