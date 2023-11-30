7d9b79eed6c0edac1e6e461ff77b0963e1e60e46
raccoonRyu <rjhwind94@gmail.com>
2023-11-30 22:35:59 +0900

    6장 while 예제 작성

---
+++ src/JavaProgrammingIntro/ch06/ch_6_6_while.java
@@ -0,0 +1,23 @@
+package JavaProgrammingIntro.ch06;
+
+public class ch_6_6_while {
+    public static void main(String[] args) {
+        int num = 0;
+        int cnt = 0;
+
+        while((num++) < 100) {
+            // if(((num % 5) != 0) || ((num % 7) != 0))
+            // continue; // 5와 7의 배수가 아니면 아래 로직을 생략\
+
+            if((num % 5) != 0)
+                continue;
+            if((num % 7) != 0)
+                continue;
+
+            cnt++;
+            System.out.println(num);
+        }
+
+        System.out.println("count : " + cnt);
+    }
+}
